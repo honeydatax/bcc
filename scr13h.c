8c5a655e92666bf53b09d837261a8ea321e473dc
honey data <16432726+honeydatax@users.noreply.github.com>
2018-05-02 11:18:32 +0000

    screen 13h sample

---
+++ scr13h.c
@@ -0,0 +1,32 @@
+#include <conio.h>
+#include <bios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <varargs.h>
+#include <stdlib.h>
+
+
+int screen13();
+void main(){
+	
+	int t;
+	
+	t=screen13();
+	
+
+
+}
+
+int screen13()
+{
+	union REGS r1;
+	union REGS r;
+	r.h.ah=0;
+	r.h.al=0x13;
+	int86(0x10,&r,&r1);
+	return r1.x.ax;
+	}
+
+
+
