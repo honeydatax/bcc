720224e741e772ae3a222169608bb38438696851
honey data <16432726+honeydatax@users.noreply.github.com>
2018-05-13 18:47:52 +0000

    graphics sample

--- gx.c
+++ gx.c
@@ -56,11 +56,11 @@ rectx=0;
 recty=0;
 rectx1=319;
 recty1=199;
-rectcolor=15;
-rect();
+cls1=1;
+cls13();
 tt=320/2;
 d=320/2+1;
-hlinecolor=b;
+hlinecolor=15;
 l=0;
 for(c=0;c<200;c++){
 	
@@ -75,6 +75,7 @@ tt=tt-1;
 }
 refresh();
 }
+
 
 int screen13()
 {
@@ -129,21 +130,20 @@ int r;
 	asm "push ds";
 	asm "push cs";
 	asm "pop ds";
-	asm "mov bx,[0x82]";
+	asm "mov di,[0x82]";
 	asm "mov dx,[0x86]";
 	
 	asm "mov cx,[0x84]";
 	asm "mov ax,[0x80]";
 	asm "push ax";
-	asm "pop ds";
+	asm "pop es";
 	asm "mov al,dl";
 	asm "xor dx,dx";
+
 asm "label2:";
-asm "mov [bx],al";
-asm "inc bx";
-asm "dec cx";
-asm "cmp cx,dx";
-	asm "jnz label2";
+asm "cld";
+asm "rep";
+asm "stosb";
 	asm "pop ds";
 			
 			
@@ -213,7 +213,7 @@ int r;
 	asm "mov cx,[0x84]";
 	asm "mov ax,[0x80]";
 	asm "push ax";
-	asm "pop ds";
+	asm "pop es";
 	asm "mov al,dl";
 	asm "xor dx,dx";
 	asm "push cx";
@@ -221,11 +221,9 @@ int r;
 	asm "pop cx";
 	asm "push cx";
 asm "label5:";
-asm "mov [di],al";
-asm "inc di";
-asm "dec cx";
-asm "cmp cx,dx";
-	asm "jnz label5";
+asm "cld";
+asm "rep";
+asm "stosb";
 	asm "clc";
 	asm "add di,si";
 asm "dec bx";
@@ -266,25 +264,25 @@ void cls13()
 	int i;
 	i=screenptr;
 	movedata(__get_ds(),&i,__get_cs(),0x80,2);
-	i=320*200;
+	i=320*200+1;
 	movedata(__get_ds(),&i,__get_cs(),0x82,2);
 	movedata(__get_ds(),&cls1,__get_cs(),0x84,1);
 	asm "push ds";
 	asm "push cs";
 	asm "pop ds";
-	asm "mov bx,[0x82]";
-	asm "mov cx,[0x84]";
+	asm "mov cx,[0x82]";
+	asm "mov dx,[0x84]";
 	asm "mov ax,[0x80]";
 	asm "push ax";
-	asm "pop ds";
+	asm "pop es";
+	asm "mov al,dl";
 	asm "xor dx,dx";
-	asm "mov al,cl";
-	
+	asm "mov di,dx";	
 asm "label1:";
-asm "mov [bx],al";
-asm "dec bx";
-asm "cmp bx,dx";
-	asm "jnz label1";
+asm "cld";
+asm "rep";
+asm "stosb";
+	
 	asm "pop ds";
 	
 	}
