b23f137d403aa9213e19f9634abfe166e82f7bcd
honey data <16432726+honeydatax@users.noreply.github.com>
2018-08-02 12:37:26 +0100

    graphics c sample

--- graphics.c
+++ graphics.c
@@ -6,39 +6,12 @@
 #include <varargs.h>
 #include <stdlib.h>
 
-int x;
-int y;
-char color;
-int draw1;
-char cls1;
-int sleep1;
-int number1;
-int number2;
-int sound1;
-int sound2;
 
 int screenptr;
-int hlinex;
-int hliney;
-int hlinex1;
-int hliney1;
-char hlinecolor;
-int rectx;
-int recty;
-int rectx1;
-int recty1;
-char rectcolor;
-char cls1;
-int sleep1;
-long get_ttimer();
-void sounds();
-void draws();
 int screen13();
-int hlines();
-int rect();
-void cls13();
-void getptr();
-void sleep();
+int hlines(hlinex,hliney,hlinex1,hliney1,hlinecolor);
+void cls13(cls1);
+int getptr();
 void refresh();
 
 
@@ -50,24 +23,19 @@ void main(){
 	char b=1;
 	
 	int t=screen13();
-getptr();
-cls1=4;
-cls13();
+screenptr=getptr();
+cls13(4);
 
-hlinex=0;
-hlinex1=1;
-hlinecolor=b;
+
+d=1;
 l=0;
 for(c=0;c<200;c++){
-hliney=c;
-hliney1=c;
-hlines();
+hlines(0,c,d,c,1);
 l=l+2100;
 ll=l/1000;
 d=ll;
 l=l-500;
 
-hlinex1=d;
 }refresh();
 }
 
@@ -81,7 +49,7 @@ int screen13()
 	return r1.x.ax;
 	}
 
-int hlines()
+int hlines(hlinex,hliney,hlinex1,hliney1,hlinecolor)
 {
 
 	int ir;
@@ -153,110 +121,8 @@ asm "cmp cx,dx";
 	}
 
 
-int rect()
-{
-	int ir;
-	int ny;
-	int nx;
-	int xx ;
-int yy;
-int xx1;
-int yy1;
-int xxx;
-int yyy;
-int r;
-	
-	int xxa;
-	xx=rectx;
-	yy=recty;
-	xx1=rectx1;
-	yy1=recty1;
-	if (xx>319) xx=319;
-	if (xx1>319) xx1=319;
-	if (yy>199) yy=199;
-	if (yy1>199) yy1=199;
-	if (xx<0) xx=0;
-	if (xx1<0) xx1=0;
-	if (yy<0) yy=0;
-	if (yy1<0) yy1=0;
-	if (xx<=xx1 && yy<=yy1) {
-		xxa=xx1-xx;
-		if (xxa<1) xxa=1;
-		yyy=yy1-yy;
-		nx=320-xxa;
-	
-			
-	xxx=yy*320+xx;
-	ir=screenptr;
-	movedata(__get_ds(),&ir,__get_cs(),0x80,2);
-	ir=xxx;
-	movedata(__get_ds(),&ir,__get_cs(),0x82,2);
-	ir=xxa;
-	movedata(__get_ds(),&ir,__get_cs(),0x84,2);
-	ir=nx;
-	movedata(__get_ds(),&ir,__get_cs(),0x86,2);
-	ir=yyy;
-	movedata(__get_ds(),&ir,__get_cs(),0x88,2);
-	movedata(__get_ds(),&rectcolor,__get_cs(),0x8a,1);
-	asm "push ds";
-	asm "push cs";
-	asm "pop ds";
-	asm "mov di,[0x82]";
-	asm "mov si,[0x86]";
-	asm "mov dx,[0x8a]";
-	asm "mov bx,[0x88]";
-	asm "mov cx,[0x84]";
-	asm "mov ax,[0x80]";
-	asm "push ax";
-	asm "pop ds";
-	asm "mov al,dl";
-	asm "xor dx,dx";
-	asm "push cx";
-	asm "label4:";
-	asm "pop cx";
-	asm "push cx";
-asm "label5:";
-asm "mov [di],al";
-asm "inc di";
-asm "dec cx";
-asm "cmp cx,dx";
-	asm "jnz label5";
-	asm "clc";
-	asm "add di,si";
-asm "dec bx";
-asm "cmp bx,dx";
-	asm "jnz label4";
-	asm "pop ax";
-	asm "pop ds";
-			
-			
-		r=-1;
 		
-		
-		}else{
-			r=0;
-			}
-		
-		return r;
-	}
-
-void sleep(){
-		unsigned long i;
-		unsigned long ii;
-		unsigned long iii;
-		iii=(long) sleep1;
-		movedata(0x40,0x6c,__get_ds(),&i,4);
-		i=iii+i;
-		do
-		{
-			movedata(0x40,0x6c,__get_ds(),&ii,4);
-			}while (ii<i) ;
-	
-		
-		
-		}
-		
-void cls13()
+void cls13(cls1)
 {
 	int i;
 	i=screenptr;
@@ -285,132 +151,11 @@ asm "cmp bx,dx";
 	}
 
 
-	long get_ttimer(){
-		long i;
-		movedata(0x40,0x6c,__get_ds(),&i,4);
-		return  i;
-		}
-		
-		int max(){
-		int r ;
-		if ( number1>number2){
-			r=number1;
-			}else{
-				r=number2;
-				}
-		return r;
-		}
-	
-	
-	int min(){
-		int r ;
-		if ( number1<number2){
-			r=number1;
-			}else{
-				r=number2;
-				}
-		return r;
-		}
-	
-	
-	void sounds(){
-		int r;
-		long f1;
-		long f2;
-		int i1;
-		int i2;
-		int i;
-		 f1 = 1193181 /sound1;
-		f2=f1;
-		f1=f1 & 0xffff0000;
-		f1=f1>>16;
-		f2=f2 & 0xffff;
-		i1=(int) f1;
-		i2=(int)f2;
-		
-		movedata(__get_ds(),&i1,__get_cs(),0x80,2);
-		movedata(__get_ds(),&i2,__get_cs(),0x82,2);
-		
-	i=0x61;
-	movedata(__get_ds(),&i,__get_cs(),0x84,2);
-	i=0x43;
-	movedata(__get_ds(),&i,__get_cs(),0x86,2);
-	i=0xb6;
-	movedata(__get_ds(),&i,__get_cs(),0x88,2);
-	i=3;
-	movedata(__get_ds(),&i,__get_cs(),0x8a,2);
-	i=0xfc;
-	movedata(__get_ds(),&i,__get_cs(),0x8c,2);
-		asm "push ds";
-		
-		asm "push cs";
-		asm "pop ds";
-		
-		asm "mov dx,[0x84]";
-		asm "mov ah,[0x8a]";
-		asm "in al,dx";
-		
-		asm "or al,ah";
-		asm "out dx,al";
-		asm "mov dx,[0x86]";
-		asm "mov al,[0x88]";
-		asm "out dx,al";
-		asm "dec dx";
-		asm "mov ax,[0x82]";
-		asm "out dx,al";
-		asm "mov al,ah";
-		asm "out dx,al";
-		asm "pop ds";
-		
-		sleep1=sound2;
-		sleep();
-		
-		i=0x61;
-	movedata(__get_ds(),&i,__get_cs(),0x84,2);
-	i=0x43;
-	movedata(__get_ds(),&i,__get_cs(),0x86,2);
-	i=0xb6;
-	movedata(__get_ds(),&i,__get_cs(),0x88,2);
-	i=3;
-	movedata(__get_ds(),&i,__get_cs(),0x8a,2);
-	i=0xfc;
-	movedata(__get_ds(),&i,__get_cs(),0x8c,2);
-		
-		asm "push ds";
-		asm "push cs";
-		asm "pop ds";
-		
-	    asm "mov dx,[0x84]";
-		asm "in al,dx";
-		asm "and al,[0x8c]";
-		asm "out dx,al";
-		
-		asm "pop ds";
-		
-		
-		}
-
-void draws(){
-rectx=x-10;
-recty=y-20;
-rectx1=x+10;
-recty1=y+20;
-rectcolor=draw1;
-rect();
-rectx=x-20;
-recty=y-10;
-rectx1=x+20;
-recty1=y+10;
-rectcolor=draw1;
-rect();
-
-
-}
 
 
 
 
-	void getptr(){
+	int getptr(){
 		
 		int r;
 		
@@ -422,7 +167,7 @@ rect();
 		asm "pop ds";
 		
 	    movedata(__get_cs(),0x80,__get_ds(),&r,2);
-		screenptr=r+0x2000;
+		return r+0x2000;
 		
 		}
 
